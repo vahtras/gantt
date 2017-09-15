ce924cbe2e2a803c24801a9221dbdbdd0cd69a34
Olav Vahtras <vahtras@kth.se>
2017-09-15 23:08:26 +0200

    fix offsets

--- gantt.py
+++ gantt.py
@@ -1,24 +1,65 @@
+import sys
+import pandas
+import seaborn
+import matplotlib.pyplot as plt
+import collections
+
+
 DAYS_MONTH = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
 MONTH_LABELS = (
     'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 
     'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
     )
+Col = collections.namedtuple('col', ['name', 'start', 'end', 'misc'])
+
 
 def project_start(df):
-    return min(df.From)
+    col = Col(*df.columns[:4])
+    return min(df[col.start])
 
 def project_end(df):
-    return max(df.To)
+    col = Col(*df.columns[:4])
+    return max(df[col.end])
 
 def start_tasks(df):
-    delta = (df.From - project_start(df))
+    col = Col(*df.columns[:4])
+    delta = (df[col.start] - project_start(df))
     days = delta.astype('timedelta64[D]')
     return days
 
 def end_tasks(df):
-    delta = (df.To - project_start(df))
+    col = Col(*df.columns[:4])
+    delta = (df[col.end] - project_start(df))
     days = delta.astype('timedelta64[D]')
     return days
 
+def plot(df):
+    col = Col(*df.columns[:4])
+    idays = start_tasks(df)
+    fdays = end_tasks(df)
+    seaborn.barplot(x=fdays, y=df[col.name])
+    seaborn.barplot(x=idays, y=df[col.name], color="#FFFFFF")
+    days_accumulated = [sum(DAYS_MONTH[:i]) for i in range(12)]
+    start_month = project_start(df).month - 1
+    end_month = project_end(df).month + 1
+    plt.xlim((days_accumulated[start_month], days_accumulated[end_month-start_month]))
+    plt.xticks(days_accumulated[:end_month-start_month:], MONTH_LABELS[start_month:end_month:])
+    plt.xlabel('')
+    plt.show()
+    
 
+def main():
+    try:
+        csv = sys.argv[1]
+    except IndexError:
+        print(f"Usage: {sys.argv[0]} csvfile")
 
+    df = pandas.read_csv(csv, parse_dates=[1, 2])
+    col = Col(*df.columns[:4])
+    df = df.sort_values(col.start)
+    print(df)
+
+    plot(df)
+
+if __name__ == "__main__":
+    main()
--- test_gantt.py
+++ test_gantt.py
@@ -1,14 +1,23 @@
 import pytest
+import unittest.mock
 import pandas as pd
+import pandas.util.testing as pdt
 import io
 
 import gantt
 
 csv = """\
-Proj,From,To
-A,2018-01-01,2018-03-31
-B,2018-04-01,2018-06-30
-C,2018-07-01,2018-12-31
+Proj,From,To,Comment
+A,2018-01-01,2018-03-31,aaa
+B,2018-04-01,2018-06-30,bbb
+C,2018-07-01,2018-12-31,ccc
+"""
+
+csv2 = """\
+Proj,From,To,Comment
+A,2018-02-01,2018-03-31,aaa
+B,2018-05-01,2018-06-30,bbb
+C,2018-08-01,2018-12-31,ccc
 """
 
 @pytest.fixture
@@ -19,17 +28,60 @@ def df():
         parse_dates=['From', 'To']
         )
 
+@pytest.fixture
+def df2():
+    csv_stream = io.StringIO(csv2)
+    return pd.read_csv(
+        csv_stream,
+        parse_dates=['From', 'To']
+        )
+
 def test_project_start(df):
     assert gantt.project_start(df) == pd.Timestamp('2018-01-01')
+
+def test_project_start2(df2):
+    assert gantt.project_start(df2) == pd.Timestamp('2018-02-01')
 
 def test_project_end(df):
     assert gantt.project_end(df) == pd.Timestamp('2018-12-31')
 
-def test_start_taskx(df):
+def test_start_tasks(df):
     days = [int(d) for d in gantt.start_tasks(df)]
     assert days == [0, 90, 181]
+
+def test_start_tasks2(df2):
+    days = [int(d) for d in gantt.start_tasks(df2)]
+    assert days == [0, 89, 181]
 
 def test_end_tasks(df):
     days = [int(d) for d in gantt.end_tasks(df)]
     assert days == [89, 180, 364]
+
+def test_end_tasks2(df2):
+    days = [int(d) for d in gantt.end_tasks(df2)]
+    assert days == [58, 149, 333]
+
+
+@unittest.mock.patch('gantt.plt.show')
+@unittest.mock.patch('gantt.seaborn.barplot')
+def test_plot(mock_plot, mock_show, df):
+
+    gantt.plot(df)
+
+    calls = [
+        unittest.mock.call(
+            x=pd.Series([89., 180., 364.]),
+            y=pd.Series(['A', 'B', 'C']),
+            ),
+#       unittest.mock.call(
+#           x=[0, 90, 181],
+#           y=['A', 'B', 'C'],
+#           color="#FFFFFF"
+#           ),
+        ]
+    mock_plot.assert_called
+    mock_show.assert_called
+    #mock_plot.assert_has_calls(calls)
+
+
 
