b058f2f259b2a9f9c8e865e56fd40cfeb3a75caa
Olav Vahtras <vahtras@kth.se>
2017-09-18 22:26:31 +0200

    update setup

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@ import setuptools
 
 setuptools.setup(
     name="minimal-gantt",
+    author="Olav Vahtras",
+    author_email="olav.vahtras@gmail.com",
+    description="A minimal Gantt chart generator",
+    license="MIT",
+    version="0.1",
     py_modules="gantt",
     install_requires=["pandas", "seaborn"],
     )
